295f0851a8de66298a02e77a2ca75516a461aacb
codyfinn <thecodyfinn@gmail.com>
2017-04-21 12:51:32 -0400

    fixed protocol mismatch for redirect_uri

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,7 @@
 OmniAuth.config.logger = Rails.logger
 
+OmniAuth.config.full_host = Rails.env.production? ? "https://rpgazebo.herokuapp.com" : "http://localhost:3000"
+
 Rails.application.config.middleware.use OmniAuth::Builder do 
   provider :google_oauth2, AppConfig[:oauth_google_client_id], AppConfig[:oauth_google_client_secret],
     {
