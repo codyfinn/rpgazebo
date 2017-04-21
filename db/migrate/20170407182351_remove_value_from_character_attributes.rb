ecc297ff875dee163198cec4f9c59aada500c01c
codyfinn <thecodyfinn@gmail.com>
2017-04-21 12:08:19 -0400

    removed migrations that are not relavant for production

--- db/migrate/20170407182351_remove_value_from_character_attributes.rb
+++
@@ -1,7 +0,0 @@
-class RemoveValueFromCharacterAttributes < ActiveRecord::Migration[5.0]
-  def change
-
-    remove_column :character_attributes, :value
-    
-  end
-end
--- db/migrate/20170407183008_add_min_and_max_to_character_attribute.rb
+++
@@ -1,6 +0,0 @@
-class AddMinAndMaxToCharacterAttribute < ActiveRecord::Migration[5.0]
-  def change
-    add_column :character_attributes, :min, :integer
-    add_column :character_attributes, :max, :integer
-  end
-end
